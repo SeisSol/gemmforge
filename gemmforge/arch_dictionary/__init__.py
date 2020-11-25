b042ba156603bd149e5cdfc113a2e0cc34a02d3e
Simon-quv <69350765+Simon-quv@users.noreply.github.com>
2020-11-25 10:02:34 +0100

    Renamed to lexicon

--- gemmforge/arch_dictionary/__init__.py
+++
@@ -1,9 +0,0 @@
-from .nvidia_arch_dictionary import NvidiaArchDictionary
-from .amd_arch_dictionary import AmdArchDictionary
-
-
-def arch_dictionary_factory(arch_name):
-    if arch_name == "nvidia":
-        return NvidiaArchDictionary()
-    elif arch_name == "amd":
-        return  AmdArchDictionary()
