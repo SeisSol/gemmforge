4b9aae66268ff8716fb44fb83ea146eb2f00dc82
Simon-quv <69350765+Simon-quv@users.noreply.github.com>
2020-11-25 10:04:06 +0100

    Updated values for amd

--- gemmforge/arch.py
+++ gemmforge/arch.py
@@ -54,8 +54,8 @@ def produce(name, sub_name):
         # warpSize equals a wavefront for AMD (Page 31 from
         # https://www.olcf.ornl.gov/wp-content/uploads/2019/10/ORNL_Application_Readiness_Workshop-AMD_GPU_Basics.pdf)
         amd_wavefront = 64
-        # PAge 16 at https://developer.amd.com/wordpress/media/2017/08/Vega_Shader_ISA_28July2017.pdf
-        max_reg_per_workgroup = 0 * KB
+        # Page 16 at https://developer.amd.com/wordpress/media/2017/08/Vega_Shader_ISA_28July2017.pdf
+        max_reg_per_workgroup = 256 * KB
         # maxThreadsPerBlock
         max_num_threads = 1024
         if sub_name in ['gfx906']:
@@ -63,8 +63,9 @@ def produce(name, sub_name):
             # Block == Workgroup(Page 31 from
             # https://www.olcf.ornl.gov/wp-content/uploads/2019/10/ORNL_Application_Readiness_Workshop-AMD_GPU_Basics.pdf)
             # (https://rocmdocs.amd.com/en/latest/Programming_Guides/Opencl-optimization.html?#specific-guidelines-for-gcn-family-gpus)
-            max_workgroup_per_cu = 16
+            max_workgroup_per_cu = 40
             # 4 SIMD Vector Units, each 16-lane wide (https://en.wikipedia.org/wiki/Graphics_Core_Next#Compute_units)
+            # Currently not used
             max_vec_units_per_cu = 64
             # sharedMemPerBlock
             max_local_mem_size_per_workgroup = 64 * KB
