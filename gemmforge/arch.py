fc4117b4cbea6e853ca9d4c6a12f8031c4e12231
Simon-quv <69350765+Simon-quv@users.noreply.github.com>
2021-02-03 10:25:59 +0100

    Added MI100 arch
    Filled in the correct values

--- gemmforge/arch.py
+++ gemmforge/arch.py
@@ -49,14 +49,16 @@ def produce(name, sub_name):
                             name)
 
     elif name == "amd":
-        # warpSize equals a wavefront for AMD (Page 31 from
-        # https://www.olcf.ornl.gov/wp-content/uploads/2019/10/ORNL_Application_Readiness_Workshop-AMD_GPU_Basics.pdf)
-        amd_wavefront = 64
-        # Page 16 at https://developer.amd.com/wordpress/media/2017/08/Vega_Shader_ISA_28July2017.pdf
-        max_reg_per_workgroup = 256 * KB
-        # maxThreadsPerBlock
-        max_num_threads = 1024
+        
         if sub_name in ['gfx906']:
+            #MI50
+            # warpSize equals a wavefront for AMD (Page 31 from
+            # https://www.olcf.ornl.gov/wp-content/uploads/2019/10/ORNL_Application_Readiness_Workshop-AMD_GPU_Basics.pdf)
+            amd_wavefront = 64
+            # Page 16 at https://developer.amd.com/wordpress/media/2017/08/Vega_Shader_ISA_28July2017.pdf
+            max_reg_per_workgroup = 256 * KB
+            # maxThreadsPerBlock
+            max_num_threads = 1024
             # a sm at Nvidia corresponds to a CU at AMD (https://en.wikipedia.org/wiki/Graphics_Core_Next#CU_scheduler)
             # Block == Workgroup(Page 31 from
             # https://www.olcf.ornl.gov/wp-content/uploads/2019/10/ORNL_Application_Readiness_Workshop-AMD_GPU_Basics.pdf)
@@ -66,6 +68,15 @@ def produce(name, sub_name):
             # Currently not used
             max_vec_units_per_cu = 64
             # sharedMemPerBlock
+            max_local_mem_size_per_workgroup = 64 * KB
+        elif sub_name in ['gfx908']:
+            #MI100
+            amd_wavefront = 64
+            max_reg_per_workgroup = 512 * KB
+            max_num_threads = 1024
+            max_workgroup_per_cu = 40
+            #Still 4 SIMD 16 Lanes Wide
+            max_vec_units_per_cu = 64
             max_local_mem_size_per_workgroup = 64 * KB
         else:
             raise ValueError(f'Given amd CU model is not supported. Provided: {sub_name}')
