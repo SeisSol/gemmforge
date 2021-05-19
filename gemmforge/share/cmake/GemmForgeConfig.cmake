eb92dbda481b93374d7f7f871c912007e38b5f61
Ludwig Kratzl <kratzl.ludwig@gmail.com>
2021-05-19 13:11:08 +0200

    Update to yateto call

--- gemmforge/share/cmake/GemmForgeConfig.cmake
+++ gemmforge/share/cmake/GemmForgeConfig.cmake
@@ -5,11 +5,14 @@ execute_process(COMMAND realpath ${CMAKE_CURRENT_LIST_DIR}/../..
 string(REGEX REPLACE "\n$" "" GEMMFORGE_PATH "${GEMMFORGE_PATH}")
 
 set(GemmForge_INCLUDE_DIRS "${GEMMFORGE_PATH}/include")
-set(GemmForge_SOURCES "${GemmForge_INCLUDE_DIRS}/gemmforge_aux.cu"
-                      "${GemmForge_INCLUDE_DIRS}/gemmforge_aux.cpp"
-                      "${GemmForge_INCLUDE_DIRS}/gemmforge_aux_sycl.cpp")
 
-
+if (${DEVICE_BACKEND} STREQUAL "CUDA")
+    set(GemmForge_SOURCES "${GemmForge_INCLUDE_DIRS}/gemmforge_aux.cu")
+elseif (${DEVICE_BACKEND} STREQUAL "HIP")
+    set(GemmForge_SOURCES "${GemmForge_INCLUDE_DIRS}/gemmforge_aux.cpp")
+else()
+    set(GemmForge_SOURCES "${GemmForge_INCLUDE_DIRS}/gemmforge_aux_sycl.cpp")
+endif()
 
 find_package_handle_standard_args(GemmForge
                                   GemmForge_INCLUDE_DIRS
