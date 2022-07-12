3f984b995e688ae7ab5f9d6a47a072658bb4c6b0
Rihab Torjmen <Torjmenrihab@gmail.com>
2022-07-12 12:26:34 +0100

    Update gemm.py

--- gemmforge/instructions/gemm.py
+++ gemmforge/instructions/gemm.py
@@ -71,7 +71,7 @@ class GenericGemm(AbstractInstruction):
           i=i+1
       elif op2_data_view.values != None and op2_data_view.is_transposed==False:
         for x in op2_data_view.spp :
-          writer(f'{self._dest.name}{[x[1]]} += {self._op1.name}[{thread_idx_x} * {op1_data_view.lead_dim} + {x[0]}] * {op2_data_view.values[i]};')
+          writer(f'{self._dest.name}{[x[1]]} += {self._op1.name}[{thread_idx_x} * {op1_data_view.lead_dim} + {x[0]}];')
           i=i+1
       elif op2_data_view.values == None and op2_data_view.is_transposed==True:
         for x in op2_data_view.spp :
