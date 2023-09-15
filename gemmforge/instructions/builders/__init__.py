7af99e331b0c6caa8aa3b325d0f2e6e9fe3b0433
Thrud Primrose <budanaz.yakup@gmail.com>
2023-09-15 17:07:59 +0200

    Fix cmake install python 3.6

--- gemmforge/instructions/builders/__init__.py
+++ gemmforge/instructions/builders/__init__.py
@@ -1,4 +1,4 @@
 from gemmforge.instructions.builders.gemms.gemm_builder import RegisterOnlyDenseGemmBuilder, ShrMemBasedDenseGemmBuilder
 from gemmforge.instructions.builders.product.product_builder import ShrMemBasedProductBuilder
-from .allocator_builder import RegistersAllocBuilder, ShrMemAllocBuilder, ShrMemNewAllocBuilder
-from .ptr_manip_builder import GetElementPtrBuilder
+from gemmforge.instructions.builders.allocator_builder import RegistersAllocBuilder, ShrMemAllocBuilder, ShrMemNewAllocBuilder
+from gemmforge.instructions.builders.ptr_manip_builder import GetElementPtrBuilder
